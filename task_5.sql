7e46bad2e7b00af4535d1ddd5ef9efd892300d8a
JumaRomano <mzitohcreatives@gmail.com>
2024-10-20 21:58:27 +0300

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -3,3 +3,5 @@ USE alx_book_store;
 -- Insert a single row into the Customers table
 INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
+mysql -u your_username -p alx_book_store < task_5.sql
