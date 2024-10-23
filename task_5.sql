8b57664977a8ab03d84040483e2c680687d4a8f4
Romano Victor <romanojuma15@gmail.com>
2024-10-23 11:44:58 +0300

    Added a missing task_2

--- task_5.sql
+++ task_5.sql
@@ -1,7 +1,9 @@
-USE alx_book_store;
-
 -- Insert a single row into the Customers table
+USE alx_book_store;
 INSERT INTO Customers (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
-
-mysql -u your_username -p alx_book_store < task_5.sql
+VALUES (
+        1,
+        'Cole Baidoo',
+        'cbaidoo@sandtech.com',
+        '123 Happiness Ave.'
+    );
