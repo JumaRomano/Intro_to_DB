633e86d025c5f89fdf1adc9cf6434e944102daaa
Romano Victor <romanojuma15@gmail.com>
2024-10-23 11:58:03 +0300

    changes done

--- task_5.sql
+++ task_5.sql
@@ -1,6 +1,5 @@
 -- Insert a single row into the Customers table
-USE alx_book_store;
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO customers (customer_id, customer_name, email, address)
 VALUES (
         1,
         'Cole Baidoo',
